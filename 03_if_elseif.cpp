ea8a0efcee3a31ab210352316a5b40482c6897ea
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-04-24 22:42:06 +0800

    if_elseif_else

---
+++ 03_if_elseif.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int x,y;
+  cout<<"ÊäÈëx£¬y£º"<<endl;
+  cin>>x>>y;
+  if(x==y)
+    cout<<"x=y"<<endl;
+  else if(x>y)
+    cout<<"x>y"<<endl;
+  else
+    cout<<"x<y"<<endl;
+  return 0;
+}
