7ea43810ca11aedf5c4b6637d5217e89e6131dac
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-04-24 23:13:37 +0800

    while

--- 05_while.cpp
+++ 05_while.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main(){
   int x,sum,y;
+  char t;
   cout<<"x:"<<endl;
   cin>>x;
   y=x;
@@ -14,11 +15,11 @@ int main(){
   cout<<"sum"<<sum<<endl;
   sum = 1;
   cout<<"do_while"<<endl;
+  y--;//加上和上面一样
   do{
     sum*=(y+1);
     cout<<"y:"<<y<<endl;
   }while(y--);
   cout<<"sum"<<sum<<endl;
-
   return 0;
 }
