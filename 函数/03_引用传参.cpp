858824da0c20786555b0143933787c75f021a229
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-04-29 19:16:53 +0800

    引用传参

---
+++ 函数/03_引用传参.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+// 形参改变实参也变
+
+void swap(int &a,int&b){
+  int c;
+  c = a;
+  a = b;
+  b = c;
+}
+
+int main(){
+  int a,b;
+  cout<<"Please input two int nums: "<<endl;
+  cin>>a>>b;
+  cout<<"a is : "<<a<<" and b is : "<<b<<endl;
+  swap(a,b);
+  cout<<"after swaped"<<"\n"<<"a is : "<<a<<" and b is : "<<b<<endl;
+
+}
