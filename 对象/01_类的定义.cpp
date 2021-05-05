0bd047fee1ddc81ea5e2771cf75910fbf3dd89a6
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-05-05 20:44:18 +0800

    类的定义

--- 对象/01_类的定义.cpp
+++ 对象/01_类的定义.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 using namespace std;
 
+// 数据成员莺歌声明成类
+class Clock{
+  public:
+    void setTime(int newH=0, int newM=0, int newS=0);
+    void showTime();
+  private:
+    int hour,minute,second;
+};//类定义后要加分号
+
+// 类声明在类里
+// 而实现是写在类定义外
+void Clock::setTime(int newH, int newM, int newS){
+  hour = newH;
+  minute = newM;
+  second = newS;
+}
+
+inline void Clock::showTime(){
+  cout<<hour<<':'<<minute<<':'<<second<<endl;
+}
+
 int main(){
+  Clock myClock;
+  cout<<"First time set and output:"<<endl;
+  myClock.setTime();
+  myClock.showTime();
+  cout<<"Second time set and output:"<<endl;
+  myClock.setTime(20,41,30);
+  myClock.showTime();
+
   return 0;
 }
