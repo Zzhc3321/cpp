acf1cc347734a042d73b9f50e445e367fbbdb22d
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-05-06 19:51:40 +0800

    构造函数的基本使用

--- 对象/02_构造与析构.cpp
+++ 对象/02_构造与析构.cpp
@@ -8,11 +8,36 @@ using namespace std;
 ④若没有定义构造函数，则编译器自动构造隐含的默认构造函数
 */
 
+class Clock{
+public:
+  Clock(int h,int m,int s);
+  void settime(int h,int m,int s);
+  void showtime();
+private:
+  int hour,minute,second;
+};
 
+// 构造函数
+Clock::Clock(int h,int m,int s){
+  hour = h;
+  minute = m;
+  second = s;
+}
 
+void Clock::settime(int h,int m,int s){
+  hour = h;
+  minute = m;
+  second = s;
+}
 
+inline void Clock::showtime(){
+  cout<<hour<<':'<<minute<<':'<<second<<endl;
+}
 
 int main(){
-
+  Clock c(0,0,0);
+  c.showtime();
+  c.settime(8,30,30);
+  c.showtime();
   return 0;
 }
