f47a244d460a40aca3d5cbccb668f10aa485d9ab
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-05-11 21:22:48 +0800

    结构体

---
+++ 对象/08_结构体.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+// 结构体与类基本一致
+// 为的是和c保持兼容
+// 结构体只允许定义数据成员，不允许函数成员
+// 数据公有
+
+// 学生信息结构体
+struct Student{
+  int num;
+  string name;
+  char gender;
+  int age;
+};
+
+int main(){
+  Student stu={185288,"zzhc",'M',21};
+  cout<<"name:"<<stu.name<<endl;
+  cout<<"student_id:"<<stu.num<<endl;
+  cout<<"age:"<<stu.age<<endl;
+  return 0;
+}
