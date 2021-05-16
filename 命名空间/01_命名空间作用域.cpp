96e74a2d2f37a5f2f8240e1ae6e651e14346a2e9
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-05-16 20:30:13 +0800

    命名空间作用域

---
+++ 命名空间/01_命名空间作用域.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int i;// 全局命名空间的全局变量
+
+namespace Ns{
+  int j;//Ns命名空间中的全局变量
+}
+
+int main(){
+  i = 5; //全局变量i赋值
+  Ns::j=6; //为全局变量j赋值
+  {//子空间
+    using namespace Ns;
+    int i;
+    i = 7;
+    cout<<"i="<<i<<endl;
+    cout<<"j="<<j<<endl;
+  }
+  cout<<"i="<<i<<endl;
+  cout<<"j="<<Ns::j<<endl;
+  return 0;
+}
