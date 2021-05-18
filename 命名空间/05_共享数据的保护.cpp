5304ab9c0b8a2cff2eb82e3d13c98175c9a205ba
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-05-18 21:00:24 +0800

    共享数据的保护

---
+++ 命名空间/05_共享数据的保护.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+// 常对象：数据成员值在对象的整个生存期间不能改变。
+// 常对象必须进行初始化，且不能被更新。
+
+// 常对象： const 类型说明符 对象名;
+// 常成员函数： 类型说明符 函数名（参数表） const;
+// 常数据成员： const 类型说明符 数据名.
+
+class A{
+public:
+  A(int i);
+  void print();
+private:
+  const int a;
+  static const int b;
+};
+
+const int A::b = 10; //静态常数据成员在类外说明和初始化
+
+A::A(int i):a(i){}//常数据成员只能通过初始化列表来获得初值
+
+void A::print(){
+  cout<<a<<":"<<b<<endl;
+}
+
+int main(){
+
+  A a1(100),a2(0);
+  a1.print();
+  a2.print();
+
+  return 0;
+}
