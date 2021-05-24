4776a149d1e9ab51f3fb33535c5db180bfd29214
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-05-24 20:40:23 +0800

    account.h

---
+++ 命名空间/07_个人银行管理程序/account.h
@@ -0,0 +1,23 @@
+class SavingAccount{
+private:
+  int id;
+  double balance;
+  double rate;
+  int lastDate;
+  double accumulation;
+  static double total;
+  void record(int date, double amount);
+  double accumulate(int date) const{
+    return accumulation+balance*(date-lastDate);
+  }
+public:
+  SavingAccount{int date.int id,double rate};
+  int getID() const {return id;}
+  double getBalance() const {return balance;}
+  double getRate() const {return rate;}
+  static double getTotal(){return total;}
+  void deposit(int date,double amount);
+  void withdraw(int date,double amount);
+  void settle(int date);
+  void show() const;
+};
