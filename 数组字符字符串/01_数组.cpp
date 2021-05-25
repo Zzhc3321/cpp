6a9d6f47c122a535839992297c3e08223a35ce9e
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-05-25 20:21:49 +0800

    数组

--- 数组字符字符串/01_数组.cpp
+++ 数组字符字符串/01_数组.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
 using namespace std;
 
+// 数组元素在内存中是顺序、连续存储的。数组元素在内存中占据一组连续的存储单元，逻辑上相邻的元素在物理地址上也是相邻的。
+
+
+// 数组作为函数参数
+// 一般不指定数组第一维的大小
+void rowSum(int a[][4],int nRow){
+  for(int i=0;i<nRow;i++){
+    for(int j=1;j<4;j++){
+      a[i][0]+=a[i][j];
+    }
+  }
+}
+
+
 int main(){
   int a[10],b[10][3];
   int i=0;
+
+  int as[][3] = {1,0,0,0,0,1};// 等于int as[2][3]={1,0,0,0,0,1}
+
+  int table[3][4] = {{1,2,3,4},{2,3,4,5},{3,4,5,6}};
+  rowSum(table,3);
+
+  for(int i=0;i<3;i++){
+    cout<<"Sum of row "<<i<<" is "<<table[i][0]<<endl;
+  }
+
 
   for(i=0;i<10;i++){
     int j=0;
@@ -20,4 +44,8 @@ int main(){
     for(j=0;j<3;j++)
           cout<<"b["<<i<<"]"<<"["<<j<<"]:"<<b[i][j]<<endl;
   }
+
+return 0;
+
+
 }
