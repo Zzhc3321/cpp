dedc5fe5106f681e957c8555834f88cd987b3f5b
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-05-31 19:47:42 +0800

    字符数组

---
+++ 数组字符字符串/10_字符数组.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+  char str[8] = {'p','r','o','g','r','\0','a','m'}; //Ä¬ÈÏµ½"\0"
+  char str1[8] = "program";
+  char str2[] = "program";
+  cout<<str<<endl;
+
+  return 0;
+}
