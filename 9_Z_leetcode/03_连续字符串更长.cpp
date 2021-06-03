f9cc3d7ba3390ecb9351226c82bb7dfd62feecfc
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-06-03 22:49:52 +0800

    连续字符串更长

---
+++ 9_Z_leetcode/03_连续字符串更长.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool checkZeroOnes(string s) {
+        int one_n=0,zero_n=0;
+        int one_t=0,zero_t=0;
+        char s0 = s[0];
+        if(s0=='1'){
+            one_t++;
+            one_n++;
+        }
+        else{
+            zero_n++;
+            zero_t++;
+        }
+        for(int i=1;i<s.size();i++){
+                if(s[i]=='1'){
+                    zero_t=0;
+                    one_t++;
+                    if(one_t>one_n)
+                     one_n=one_t;
+                }
+                else{
+                    one_t=0;
+                    zero_t++;
+                    if(zero_t>zero_n)
+                    zero_n = zero_t;
+                }
+        }
+        cout<<zero_n<<one_n<<endl;
+        return one_n>zero_n;
+    }
+};
+
+int main(){
+Solution slove;
+cout<<slove.checkZeroOnes("01111110")<<endl;
+  return 0;
+}
