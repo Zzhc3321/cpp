41d733f42db5cf8438d73f07357be22e240d6ace
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-06-05 23:03:43 +0800

    删除最小值并用最后一个值填充

---
+++ data_structure/线性表顺序/02_删除最小值.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+const int maxsize=15;
+typedef int ElemType;
+
+struct List{
+    ElemType data[maxsize];
+    int length;
+};
+
+bool Solution(List &L,ElemType &min_data){
+    min_data=L.data[0];
+    int pos=0;
+    for(int i=1;i<L.length;i++)
+        if(min_data>L.data[i]){
+            min_data = L.data[i];
+            pos = i;
+        }
+    L.data[pos] = L.data[L.length-1];
+    L.length--;
+    return true;
+}
+
+void ShowElem(List L){
+  for(int i=0;i<L.length;i++)
+    cout<<L.data[i]<<' ';
+  cout<<endl;
+}
+
+int main(){
+    List LS;
+    cout<<"Enter an intger number which is below than "<<maxsize<<" : ";
+    cin>>LS.length;
+    for(int i=0;i<LS.length;i++){
+        cout<<"Enter data "<<i+1<<" : ";
+        cin>>LS.data[i];
+    }
+    ShowElem(LS);
+    ElemType res;
+    Solution(LS,res);
+    cout<<"The min elem is : "<<res<<endl;
+    cout<<"The result of this solution is : ";
+    ShowElem(LS);
+
+
+  return 0;
+}
