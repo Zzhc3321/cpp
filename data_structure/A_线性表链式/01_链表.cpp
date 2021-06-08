ce7890e5e414c084e23049acb6fd9c39e177f0e8
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-06-08 23:17:58 +0800

    链表操作

--- data_structure/A_线性表链式/01_链表.cpp
+++ data_structure/A_线性表链式/01_链表.cpp
@@ -39,15 +39,6 @@ void Create_list_tail(Llist &L){
   r->next = NULL;
 }
 
-void ShowElem(Llist L){
-  LNode *p = L->next;
-  while(p->next!=NULL){
-    cout<<p->data<<' ';
-    p = p->next;
-  }
-  cout<<endl;
-}
-
 bool GetElem(Llist L,int i,LNode *res){
   LNode *p=L;
   while(p!=NULL){
@@ -63,6 +54,59 @@ bool GetElem(Llist L,int i,LNode *res){
   }
   return true;
 }
+
+bool Delete_elem_by_position(Llist &L,int i){
+  if(i<=0)
+    return false;
+  LNode *p = L;
+  for(int j=0;j<i-1;j++){
+    p = p->next;
+    if(p==NULL)
+      return false;
+  }
+  LNode *s = p->next;
+  p->next = p->next->next;
+  delete s;
+  return true;
+}
+
+bool Delete_elem_by_Elem(Llist &L,Elemtype e){
+  LNode *p=L;
+  while(p->next!=NULL){
+    LNode *s=p->next;
+    if(s->data==e){
+      p->next = s->next;
+      delete s;
+    }
+    else
+      p = p->next;
+  }
+  return true;
+}
+
+bool Insert_elem(Llist &L,Elemtype e,int i){
+  LNode *p=L,*s;
+  for(int j=1;j<i;j++){
+    p = p->next;
+    if(p==NULL)
+      return false;
+  }
+  s = new(LNode);
+  s->data = e;
+  s->next = p->next;
+  p -> next = s;
+  return true;
+}
+
+void ShowElem(Llist L){
+  LNode *p = L->next;
+  while(p!=NULL){
+    cout<<p->data<<' ';
+    p = p->next;
+  }
+  cout<<endl;
+}
+
 
 int main(){
   // Llist L;
@@ -80,5 +124,13 @@ int main(){
     cout<<node->data<<endl;
   else
     cout<<"something wrong"<<endl;
+  ShowElem(L);
+  // Delete_elem_by_position(L,5);
+  Delete_elem_by_Elem(L,6);
+  ShowElem(L);
+
+  Insert_elem(L,6,5);
+  ShowElem(L);
+
   return 0;
 }
