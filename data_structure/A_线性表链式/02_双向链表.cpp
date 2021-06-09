33d4727c2387d426f1146048eefcc29c394e58d8
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-06-09 23:24:29 +0800

    双向链表

--- data_structure/A_线性表链式/02_双向链表.cpp
+++ data_structure/A_线性表链式/02_双向链表.cpp
@@ -16,7 +16,25 @@ bool Init_Two_way_Link_list(Llist &L){
   return true;
 }
 
-void ShowElem(Llist L){
+bool Create_Link_list(Llist &L){
+  LNode *p=L,*s;
+  Elemtype e;
+  cin>>e;
+  while(e!=999){
+    s = new(LNode);
+    s->data = e;
+    s->next = p->next;
+    s->prior = p;
+    p->next = s;
+    if(s->next)
+        s->next->prior = s;
+    p = s;
+    cin>>e;
+  }
+  return true;
+}
+
+void ShowElem_by_next(Llist L){
   LNode *p = L->next;
   while(p!=NULL){
     cout<<p->data<<' ';
@@ -25,17 +43,27 @@ void ShowElem(Llist L){
   cout<<endl;
 }
 
+void ShowElem_by_prior(Llist L){
+  LNode *p = L;
+  while(p->next!=NULL){
+    cout<<p->next->data<<' ';
+    p = p->next;
+  }
+  cout<<endl;
+  while(p->prior!=NULL){
+    cout<<p->data<<' ';
+    p = p->prior;
+  }
+  cout<<endl;
+}
+
 int main(){
   Llist L;
   Init_Two_way_Link_list(L);
-  // LNode *e;
-  LNode *e;
-  e = new(LNode);
-  e -> data = 1;
-  L->next = e;
-  e->prior = L;
+  cout<<"please enter elms (end by 999):"<<endl;
+  Create_Link_list(L);
 
-  ShowElem(L);
+  ShowElem_by_prior(L);
 
   return 0;
 }
