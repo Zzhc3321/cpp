950b7651aecdc764fe43ae00648392346b8fbc7c
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-06-11 22:47:58 +0800

    循环单链表

--- data_structure/A_线性表链式/03_循环单链表.cpp
+++ data_structure/A_线性表链式/03_循环单链表.cpp
@@ -8,14 +8,74 @@ typedef struct LNode{
   struct LNode *next;
 }LNode,*Llist;
 
-bool Init_circle_link_list(&L){
+bool Init_circle_link_list(Llist &L){
   L = new(LNode);
   L->next = L;
+  return true;
+}
+
+bool Create_Linklist(Llist &L){
+  LNode *s,*r=L;
+  Elemtype e;
+  cin>>e;
+  while(e!=999){
+    s = new(LNode);
+    s->data = e;
+    r->next = s;
+    r = s;
+    cin>>e;
+  }
+  r->next = L;
+  return true;
+}
+
+bool Insert_elem(Llist &L,LNode *node,int po){
+  LNode *p=L;
+  while(--po){
+    p = p->next;
+  }
+  node->next = p->next;
+  p->next = node;
+  return true;
+}
+
+bool Delete_elem_by_Elem(Llist &L,Elemtype e){
+  LNode *p=L;
+  while(p->next != L){
+    if(p->next->data==e){
+      LNode *f=p->next;
+      p->next = p->next->next;
+      delete f;
+    }
+    else
+      p = p->next;
+  }
+  return true;
+}
+
+bool ShowElem(Llist L,int num){
+  LNode *p=L;
+  while(num--){
+    if(p==L)
+      p = p->next;
+    cout<<p->data<<' ';
+    p = p->next;
+  }
+  cout<<endl;
+  return true;
 }
 
 int main(){
-  Llist = L;
+  Llist L;
   Init_circle_link_list(L);
-  
+  Create_Linklist(L);
+  ShowElem(L,50);
+  LNode *e;
+  e = new(LNode);
+  e->data = 6;
+  Insert_elem(L,e,e->data);
+  ShowElem(L,50);
+  Delete_elem_by_Elem(L,e->data);
+  ShowElem(L,50);
   return 0;
 }
