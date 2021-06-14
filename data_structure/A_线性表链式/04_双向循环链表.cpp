aba53cf7cf4144cf6b2a6a2c67d78d19c34e4484
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-06-14 22:27:34 +0800

    双向循环链表

---
+++ data_structure/A_线性表链式/04_双向循环链表.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+
+typedef int Elemtype;
+
+typedef struct LNode{
+  Elemtype data;
+  struct LNode *prior;
+  struct  LNode *next;
+}LNode,*Llist;
+
+void Init_two_way_circle_link_list(Llist &L){
+  L = new(LNode);
+  L->prior = L;
+  L->next = L;
+}
+
+bool Create_Linklist(Llist &L){
+    LNode *p=L,*s;
+    Elemtype e;
+    cin>>e;
+    while(e!=999){
+      s = new(LNode);
+      s->data = e;
+      s->next = p->next;
+      s->prior = p;
+      p->next->prior = s;
+      p->next = s;
+      p = s;
+      cin>>e;
+    }
+    return true;
+}
+
+bool ShowElem_by_next(Llist L){
+  LNode *p=L->next;
+  while(p!=L){
+    cout<<p->data<<' ';
+    p = p->next;
+  }
+  cout<<endl;
+  return true;
+}
+
+bool ShowElem_by_prior(Llist L){
+  LNode *p=L->prior;
+  while(p!=L){
+    cout<<p->data<<' ';
+    p = p->prior;
+  }
+  cout<<endl;
+  return true;
+}
+
+int main(){
+  Llist L;
+  Init_two_way_circle_link_list(L);
+  Create_Linklist(L);
+  ShowElem_by_next(L);
+  ShowElem_by_prior(L);
+  return 0;
+}
