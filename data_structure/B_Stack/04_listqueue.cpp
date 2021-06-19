b36fad93ef366d067f543bc35bc834e5565171be
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-06-19 20:06:50 +0800

    listqueue

--- data_structure/B_Stack/04_listqueue.cpp
+++ data_structure/B_Stack/04_listqueue.cpp
@@ -6,19 +6,59 @@ typedef int Elemtype;
 typedef struct Node{
   Elemtype data;
   Node* next;
-}Node,*Qnode;
+}*Qnode;
 
 typedef struct queuqe{
   Qnode front;
   Qnode rear;
-}*Que;
+}queuqe,*Que;
 
-bool Init_Q(){
+bool Init_Q(Que &Q){
+  Q = new queuqe;
+  Q->front = Q->rear = new Node;
+  Q->front->next = NULL;
+  return true;
+}
+
+bool Is_empty(Que &Q){
+  if(Q->front==Q->rear)
+    return true;
+  return false;
+}
+
+bool Enqueue(Que &Q,Elemtype e){
+  Qnode s;
+  s->data = e;
+  s->next = NULL;
+  Q->rear->next = s;
+  Q->rear = s;
+  return true;
+}
+
+bool Dequeue(Que &Q,Elemtype &e){
+  if(Is_empty(Q))
+    return false;
+  Qnode p=Q->front->next;
+  e = p->data;
+  Q->front->next = p->next;
+  if(Q->rear==p)
+    Q->front=Q->rear;
+  delete p;
   return true;
 }
 
 int main(){
   Que Q;
+  Init_Q(Q);
+  cout<<Is_empty(Q)<<endl;
+  Enqueue(Q,10);
+  Enqueue(Q,12);
+  cout<<Is_empty(Q)<<endl;
+  Elemtype e;
+
+  Dequeue(Q,e);
+  cout<<e<<endl;
+
 
   return 0;
 }
