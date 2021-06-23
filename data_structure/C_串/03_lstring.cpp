17a16d06eedf81b1da9d3fcfe1fd2d24be07b624
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-06-23 20:33:03 +0800

    动态串

---
+++ data_structure/C_串/03_lstring.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+typedef char Elemtype;
+
+typedef struct lstring{
+   Elemtype ch[4];
+   lstring *next;
+}lstring,*head;
+
+int main(){
+
+  return 0;
+}
