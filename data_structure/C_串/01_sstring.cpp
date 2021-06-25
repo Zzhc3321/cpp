348a9d0e83c0ce2440dfff21d8d871fe522a8434
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-06-25 22:21:00 +0800

    sstring

--- data_structure/C_串/01_sstring.cpp
+++ data_structure/C_串/01_sstring.cpp
@@ -9,24 +9,57 @@ typedef struct sstring{
   int length;
 }sstring;
 
-bool Substring(sstring S,sstring &s,int pos,int len){
-  if(pos+len-1>S.length)
-    return false;
-  for(int i=pos;i<pos+len;i++)
-    s.ch[i-pos+1] = S.ch[i];
-  s.length = len;
+bool StrAssign(sstring &S,sstring s){//赋值操作
+
+  return true;
+}
+
+bool StrCopy(sstring &S,sstring s){//赋值
+
+  return true;
+}
+
+bool StrEmpty(sstring S){//判空
+
+  return true;
+}
+
+bool StrCompare(sstring S,sstring s){//比较
+
+  return true;
+}
+
+bool StrLen(sstring S,int &len){//长度
+
+  return true;
+}
+
+bool Substring(sstring S,sstring &s,int pos,int len){//求字串
+
+  return true;
+}
+
+bool Concat(sstring &S,sstring s1,sstring s2){//连接
+
+  return true;
+}
+
+bool Index(sstring S,sstring T){//匹配
+
+  return true;
+}
+
+bool ClearString(sstring &S){//清空操作
+
+  return true;
+}
+
+bool DestoryString(sstring &S){//销毁
+
   return true;
 }
 
 int main(){
-  sstring S;
-  for(int i=0;i<5;i++)
-    S.ch[i] = 96+i;
 
-  cout<<S.ch<<endl;
-  S.length = 5;
-  sstring s;
-  Substring(S,s,0,2);
-  cout<<s.ch<<endl;
   return 0;
 }
