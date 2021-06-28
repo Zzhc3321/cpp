d9ce8ef473f82684e887a280472cab384f8b6131
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-06-28 17:26:49 +0800

    运算符重载

---
+++ 多态/02_运算符重载.cpp
@@ -0,0 +1,34 @@
+#include <iostream.h>
+using namespace std;
+
+typedef unsigned short USHORT;
+
+class Counter
+{
+public:
+  Counter();
+  Counter(USHORT initialValue);
+  ~Counter(){}
+  USHORT GetItsVal()const { return itsVal; }
+  void SetItsVal(USHORT x) {itsVal = x; }
+  Counter operator+ (const Counter &);
+private:
+  USHORT itsVal;
+};
+
+Counter::Counter(USHORT initialValue):itsVal(initialValue){}
+
+Counter::Counter():itsVal(0){}
+
+Counter Counter::operator+ (const Counter & rhs){
+  return Counter(itsVal + rhs.GetItsVal());//返回无明对象
+}
+
+int main(){
+  Counter varOne(2)， varTwo(4)， varThree;
+  varThree = varOne + varTwo;
+  cout << "varOne: " << varOne.GetItsVal()<< endl;
+  cout << "varTwo: " << varTwo.GetItsVal() << endl;
+  cout << "varThree: " << varThree.GetItsVal() << endl;
+  return 0;
+}
