6f0501a92443c1ddcea4687d4ed32f2fe62accef
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-06-28 17:41:16 +0800

    友元重载运算符

---
+++ 多态/04_友元重载运算符.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class Point
+{
+public:
+  Point() { X = Y = 0; }
+  Point( unsigned x, unsigned y ) { X = x; Y = y; }
+  unsigned x() { return X; }
+  unsigned y() { return Y; }
+  void Print() { cout << "Point(" << X << ", " << Y << ")" << endl; }
+  friend Point operator+( Point& pt, int nOffset );
+  friend Point operator+( int nOffset, Point& pt );
+private:
+  unsigned X;
+  unsigned Y;
+};
+
+Point operator+( Point& pt, int nOffset ){
+  Point ptTemp = pt;
+  ptTemp.X += nOffset;
+  ptTemp.Y += nOffset;
+  return ptTemp;
+}
+
+Point operator+( int nOffset, Point& pt ){
+  Point ptTemp = pt;
+  ptTemp.X += nOffset;
+  ptTemp.Y += nOffset;
+  return ptTemp;
+}
+
+int main(){
+  Point pt( 10, 10 );
+  pt.Print();
+
+  pt = pt + 5; // Point + int
+  pt.Print();
+
+  pt = 10 + pt; // int + Point
+  pt.Print();
+  return 0;
+}
