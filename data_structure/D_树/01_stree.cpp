5e8a2dce313a294ae90d26014e0ea300f4542e07
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-06-29 17:42:59 +0800

    顺序存储二叉树

--- data_structure/D_树/01_stree.cpp
+++ data_structure/D_树/01_stree.cpp
@@ -10,8 +10,28 @@ typedef struct{
   bool isEmpty;
 }TreeNode;
 
+bool get_lchild(TreeNode t[],int father_pos,Elemtype &data){
+  if(t[2*father_pos].isEmpty)
+    return false;
+  else
+    data = t[2*father_pos].value;
+  return true;
+}
+
+bool get_rchild(TreeNode t[],int father_pos,Elemtype &data){
+  if(t[2*father_pos+1].isEmpty)
+    return false;
+  else
+    data = t[2*father_pos+1].value;
+  return true;
+}
+
+bool get_fahter(TreeNode t[],int this_pos,Elemtype &data){
+  data = t[this_pos/2].value;
+  return true;
+}
+
 int main(){
   TreeNode t[Max_size];
-
   return 0;
 }
