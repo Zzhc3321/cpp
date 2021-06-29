e78b34b9e3464b75c1dc9cd44b4dc9baf0dc6872
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-06-29 17:26:21 +0800

    顺序存储二叉树

---
+++ data_structure/D_树/01_stree.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+// 完全二叉树的顺序存储
+const int Max_size = 100;
+
+typedef int Elemtype;
+
+typedef struct{
+  Elemtype value;
+  bool isEmpty;
+}TreeNode;
+
+int main(){
+  TreeNode t[Max_size];
+
+  return 0;
+}
