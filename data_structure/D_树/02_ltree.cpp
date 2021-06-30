64da60ff45a3c45ea76feaebaae3f0e7a15b11e2
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-06-30 17:25:56 +0800

    树的先中后序遍历

--- data_structure/D_树/02_ltree.cpp
+++ data_structure/D_树/02_ltree.cpp
@@ -10,11 +10,47 @@ typedef struct BiTNode{
   BiTNode *lchild, *rchild;
 }BiTNode,*BiTree;
 
+void PreOrder(BiTree T){
+  if(T!=NULL){
+    cout<<T->data<<' ';
+    PreOrder(T->lchild);
+    PreOrder(T->rchild);
+  }
+}
+
+void MedOrder(BiTree T){
+  if(T!=NULL){
+    MedOrder(T->lchild);
+    cout<<T->data<<' ';
+    MedOrder(T->rchild);
+  }
+}
+
+void BaOrder(BiTree T){
+  if(T!=NULL){
+    BaOrder(T->lchild);
+    BaOrder(T->rchild);
+    cout<<T->data<<' ';
+  }
+}
+
 int main(){
   BiTree root;
   root = new BiTNode;
-  root->data = 1;
-  root->lchild = NULL;
-  root->rchild = NULL;
+  root->data = 0;
+  root->lchild = new BiTNode;
+  root->rchild = new BiTNode;
+  root->lchild->data = 1;
+  root->rchild->data = 2;
+  root->lchild->lchild = NULL;
+  root->lchild->rchild = NULL;
+  root->rchild->lchild = NULL;
+  root->rchild->rchild = NULL;
+  PreOrder(root);
+  cout<<endl;
+  MedOrder(root);
+  cout<<endl;
+  BaOrder(root);
+  cout<<endl;
   return 0;
 }
