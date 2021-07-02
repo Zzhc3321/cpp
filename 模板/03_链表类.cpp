e4f8c630702ced33d9dfb43421cb0fdaad32df92
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-07-02 16:33:05 +0800

    链表类

--- 模板/03_链表类.cpp
+++ 模板/03_链表类.cpp
@@ -1,31 +1,6 @@
 #include <iostream>
+#include "Node.h"
 using namespace std;
-
-template <class T>
-
-class Node{
-private:
-  Node<T> *next;
-public:
-  T data;
-  Node(const T& item,Node<T>*next = 0);
-  void insertAfter(Node<T> *p);
-  Node<T> *deleteAfter();
-  Node<T> *nextNode() const;
-};
-
-void Node<T>::insertAfter(Node<T> *p){
-  p->next = next;
-  next = p;
-}
-
-Node<T> *Node<T>::deleteAfter(void){
-  Node<T>*tempter = next;
-  if(next==0)
-    return 0;
-  next = tempter->next;
-  return tempter;
-}
 
 int main(){
 
