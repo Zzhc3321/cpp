39f14831b5798a5279fdbf7783d3485caefc9316
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-07-06 17:38:40 +0800

    输出精度

---
+++ 流/03_精度.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+int main(){
+  double values[]={1.23,35.36,635.7};
+  for(int i=0;i<3;i++){
+    cout.width(10);
+    cout.fill('*');
+    cout<<setprecision(1)<<values[i]<<endl;
+  }
+  cout.put('1');
+  return 0;
+}
