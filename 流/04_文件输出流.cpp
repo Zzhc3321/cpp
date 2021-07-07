6698cfe65b6f70fbb370936622df0532711194da
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-07-07 15:31:32 +0800

    文件输出流

--- 流/04_文件输出流.cpp
+++ 流/04_文件输出流.cpp
@@ -3,10 +3,13 @@
 using namespace std;
 
 struct Date{
-  int day,year;
-}
+  char day,year;
+};
 
 int main(){
-
+  Date dt =  {'t','y'};
+  ofstream file("afile.dat",ios_base::binary);
+  file.write(reinterpret_cast<char*>(&dt),sizeof(dt));
+  file.close();
   return 0;
 }
