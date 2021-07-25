aabdeb8c779166e86878a29c2cd9ae4c6d6494aa
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-07-25 23:09:27 +0800

    0006-ZigZag-Conversion

--- 9_Z_leetcode/0006-ZigZag-Conversion.cpp
+++ 9_Z_leetcode/0006-ZigZag-Conversion.cpp
@@ -3,26 +3,35 @@
 using namespace std;
 
 string convert(string s, int numRows){
-  char res[numRows][s.length()]={{" "}};
+  string res[numRows]={" "};
+  for(int p=0;p<numRows;p++)
+  for(int t=0;t<s.length();t++)
+    res[p]+=' ';
   int i=0,j=0,k=0;
-  // for(j=0;k<s.length();j++)
-  //   for(i=0;i<numRows && k<s.length();i++){
-  //     if(j%(numRows-1)==0)
-  //       res[i][j]=s[k++];
-  //     else
-  //       res[2-i][j++] = s[k++];
-  // }
+  for(j=0;k<s.length();j++)
+    for(i=0;i<numRows && k<s.length();i++){
+      if(j%(numRows-1)==0)
+        res[i][j]=s[k++];
+      else{
 
+        cout<<numRows-2-i<<' ';
+        res[numRows-2-i][j++] = s[k++];
+      }
+
+  }
+
+  string result = "";
   for(int m=0;m<numRows;m++){
-    for(int n=0;n<s.length();n++){
-      cout<<res[m][n];
+    for(int n=0;n<res[m].length();n++){
+      if(res[m][n]!=' ')
+      result += res[m][n];
     }
-    cout<<endl;
     }
-    return s;
+    return result;
 }
 
 int main(){
-convert("PAHNAPLSIIGYIR",3);
+string res = convert("PAYPALISHIRING",4);
+cout<<res<<endl;
   return 0;
 }
