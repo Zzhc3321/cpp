2697c253aff9cefe4b259752af907ff13222833e
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-08-11 22:56:38 +0800

    最长公共前缀

---
+++ 9_Z_leetcode/0014_公共前缀.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+string longestCommonPrefix(vector<string>& strs) {
+  if (!strs.size()) {
+      return "";
+  }
+
+  string res="";
+  int c = strs[0][0];
+  int j=0;
+  for(int i=0;i<strs.size();i++){
+    if(c!=strs)
+  }
+}
+
+int main(){
+  string s="test",r="test12";
+  string a = r-s;
+  cout<<a<<endl;
+  return 0;
+}
