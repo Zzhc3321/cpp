8a251a75ea5310af7a79ca2e76917ff7e0afb17b
Zzhc3321 <58225597+Zzhc3321@users.noreply.github.com>
2021-08-14 16:24:25 +0800

    最长公共前缀

--- 9_Z_leetcode/0014_公共前缀.cpp
+++ 9_Z_leetcode/0014_公共前缀.cpp
@@ -7,18 +7,32 @@ string longestCommonPrefix(vector<string>& strs) {
   if (!strs.size()) {
       return "";
   }
-
-  string res="";
-  int c = strs[0][0];
-  int j=0;
-  for(int i=0;i<strs.size();i++){
-    if(c!=strs)
+  if (strs.size()==1){
+    return strs[0];
   }
+
+  string res=strs[0];
+  for(int i=1;i<strs.size();i++){
+    int len1=res.length();
+    int len2=strs[i].length();
+    int len = len1>len2? len2:len1;
+    string t="";
+    for(int j=0;j<len;j++){
+      if(res[j]==strs[i][j]){
+        t+=res[j];
+      }
+      else
+       break;
+    }
+    res = t;
+  }
+  return res;
 }
 
 int main(){
-  string s="test",r="test12";
-  string a = r-s;
-  cout<<a<<endl;
+  vector<string> s(2);
+  s[0] = "test";
+  s[1] = "tetss";
+  cout<<longestCommonPrefix(s)<<endl;
   return 0;
 }
